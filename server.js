182e532d6423d794ffaf203c504ba72790987e7f
alya <aliya2002mah@gmail.com>
2025-08-05 13:17:33 +0300

    .

--- server.js
+++ server.js
@@ -6,19 +6,30 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 
+// Controllers
+const testJwtRouter = require('./controllers/test-jwt');
+
+// DB connection
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
-// Routes
+// App.use routes
+app.use('/test-jwt', testJwtRouter);
+
+// Public routes
 
 
+// Protected routes
+
+// Server
 app.listen(3000, () => {
   console.log('The express app is ready!');
 });
